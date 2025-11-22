d87ad3c870dab0760afef90a91e809aa212fa008
dankukennedy <dankukennedy@gmail.com>
2025-11-22 14:08:52 +0000

    new changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,33 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import  { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
-
+    constructor(private readonly usersService:UsersService){}
 
     @Get() //GET /users or /users?role=value&ute
     findAll(@Query('role') role?:'INTERN'|'ENGINEER'|'ADMIN'){
-        return []
+        return this.usersService.findAll(role)
     }
 
     @Get (':id') //GET /users/:id
     findOne(@Param('id') id:string){
-        return {id}
+        return this.usersService.findOne(+id)
     }
 
     @Post() //POST /users
-    create(@Body() user:{}){
-        return user
+    create(@Body() user:{name:string, email:string, role:'INTERN'|'ENGINEER'|'ADMIN'}){
+        return this.usersService.create(user)
     }
 
     @Patch(':id') //PATCH /user:id
-    update(@Param('id') id:string, @Body() userUpdate:{}){
-        return {id, ...userUpdate}
+    update(@Param('id') id:string, @Body() userUpdate:{name?:string, email?:string, role?:'INTERN'|'ENGINEER'|'ADMIN'}){
+        return this.usersService.update(+id, userUpdate)
     }
-    
+
     @Delete (':id') //DELETE /user:id
     delete(@Param('id') id:string){
-       return {id}
+       return this.usersService.delete(+id)
     }
-    
+
 }
